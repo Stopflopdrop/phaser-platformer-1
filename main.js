f39cef3f1852273c4d118ac47aad54aa72d19efc
zach <Frenchzj@s.dcsdk12.org>
2023-11-14 15:45:48 -0700

    new

--- main.js
+++ main.js
@@ -12,10 +12,19 @@ const PLAYER_ANIMS = {
 class MainScene extends Phaser.Scene {
 	constructor() {
 		super("main-scene");
+
+		this.map;
 	}
 
 	preload() {
 		this.load.atlas("robot", "robot.png", "robot.json");
+
+		this.load.image("marble", "tilesets/marble.png");
+		this.load.image("rock", "tilesets/rock.png");
+		this.load.image("sand", "tilesets/sand.png");
+		this.load.image("stone", "tilesets/stone.png");
+
+		this.load.tilemapTiledJSON("map", "tilesets/mapmapmapmapmapmap2.json");
 	}
 
 	create() {
@@ -24,6 +33,28 @@ class MainScene extends Phaser.Scene {
 
 		// object destructuring
 		const { height, width } = this.scale;
+
+		this.map = this.make.tilemap({key: "map"});
+
+		const marbleTiles = this.map.addTilesetImage("marble", "marble");
+		const rockTiles = this.map.addTilesetImage("rock", "rock");
+		const sandTiles = this.map.addTilesetImage("sand", "sand");
+		const stoneTiles = this.map.addTilesetImage("stone", "stone");
+
+		this.map.createLayer(
+			"background layer",
+			[marbleTiles, rockTiles, sandTiles, stoneTiles],
+			0,
+			0
+		);
+
+
+		const platformlayer = this.map.createLayer(
+			"Platform",
+			[marbleTiles, rockTiles, sandTiles, stoneTiles],
+			0,
+			0
+		);
 
 		let player = this.physics.add.sprite(
 			width / 2,
@@ -70,6 +101,11 @@ class MainScene extends Phaser.Scene {
 			frameRate: 10, // frames per second
 			repeat: -1, // infinite repeat
 		});
+
+
+		
+
+
 		player.anims.create({
 			key: PLAYER_ANIMS.cheer,
 			frames: player.anims.generateFrameNames("robot", {
@@ -83,6 +119,10 @@ class MainScene extends Phaser.Scene {
 		});
 
 		player.play(PLAYER_ANIMS.run);
+
+		this.cursors = this.input.keyboard.addKey({
+			left: Phaser.Input.Keyboard
+		})
 	}
 
 	update() {}
@@ -91,8 +131,8 @@ class MainScene extends Phaser.Scene {
 /** @type {Phaser.Types.Core.GameConfig} */
 const config = {
 	type: Phaser.WEBGL,
-	width: 400,
-	height: 400,
+	width: 40 * 18,
+	height: 30 * 18,
 	scene: [MainScene],
 	physics: {
 		default: "arcade",
